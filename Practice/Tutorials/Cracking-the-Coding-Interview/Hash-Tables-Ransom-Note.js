aac569d07fdba74f2478334e7fe8c5577d958419
Schoolbox_95 <bpudsey@qmail.qcc.edu>
2021-01-02 13:05:29 -0500

    hash-table-ransom-note

---
+++ Practice/Tutorials/Cracking-the-Coding-Interview/Hash-Tables-Ransom-Note.js
@@ -0,0 +1,51 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the checkMagazine function below.
+function checkMagazine(magazine, note) {
+  let h = {}
+  let res = 'Yes'
+  magazine.map( m => h[m]= (h[m]) ? h[m]+1 : 1)
+  for ( let n of note) {
+    if (!h[n]) {
+      res = 'No'
+      break;
+    } else h[n]--
+  }
+  console.log(res)
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
