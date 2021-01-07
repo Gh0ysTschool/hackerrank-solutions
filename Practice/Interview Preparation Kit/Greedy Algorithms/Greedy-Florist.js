a9174ebe6dc3b54dc17575a0208ed33a65a9b2e7
Schoolbox_95 <bpudsey@qmail.qcc.edu>
2021-01-07 13:55:32 -0500

    greedy-florist

---
+++ Practice/Interview Preparation Kit/Greedy Algorithms/Greedy-Florist.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the getMinimumCost function below.
+function getMinimumCost(k, c) {
+  let cc = c.sort((a,b) => (a<b) ? 1 : (a>b) ? -1 : 0)
+  return Array(Math.ceil(cc.length/k)).fill(0).map( (v,i) => i*k ).map( i =>
+    Math.floor((i/k)+1) * cc.slice(i,i+k).reduce((a,c)=>a+Number(c),0)
+  ).reduce((a,c)=>a+Number(c),0)
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const minimumCost = getMinimumCost(k, c);
+
+    ws.write(minimumCost + '\n');
+
+    ws.end();
+}
