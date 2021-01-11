61194ffd5e0c3d43f139635e209ada76694de277
Schoolbox_95 <bpudsey@qmail.qcc.edu>
2021-01-11 14:54:58 -0500

    sales-by-match

---
+++ Practice/Interview Preparation Kit/Warm-up Challenges/Sales-by-Match.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function sockMerchant(n, ar) {
+    const dict = {}
+    let pairs = 0
+    ar.map( (el,i) => dict[el] = (dict[el]) ? dict[el]+1 : 1 )
+    Object.keys(dict).map( k => pairs += (dict[k]-(dict[k]%2)) / 2)
+    return pairs
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = sockMerchant(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
